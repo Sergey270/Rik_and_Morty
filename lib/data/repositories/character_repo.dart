12966da1a9b55c5a75a2366ff2db692dfb710ba0
2707066 <80663695+Sergey270@users.noreply.github.com>
2023-07-07 17:56:09 +0600

    add Image

--- lib/data/repositories/character_repo.dart
+++ lib/data/repositories/character_repo.dart
@@ -4,7 +4,7 @@ import 'package:rik_and_morty/data/models/character.dart';
 import 'package:http/http.dart' as http;
 
 class CharacterRepo {
-final url = 'https://rickandmortyapi.com/api/character';
+final url = "https://rickandmortyapi.com/api/character";
 
   Future<Character>getCharacter(int page, String name) async {
     try{ var response = await http.get(Uri.parse( '$url?page=$page&name=$name'));
--- lib/ui/widgets/custom_list_title.dart
+++ lib/ui/widgets/custom_list_title.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../data/models/character.dart';
@@ -9,8 +10,39 @@ class CustomListTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  ListTile(
-      title: Text(result.name, style: const TextStyle(color: Colors.white),),
+    return  ClipRRect(
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        height: MediaQuery.of(context).size.height / 7,
+        color: Colors.grey,
+        child: Row(
+          children: [
+            CachedNetworkImage(
+              imageUrl: result.image,
+              placeholder: (context, url) => const CircularProgressIndicator(color: Colors.cyan,),
+              errorWidget: (context, url, error)=> const Icon(Icons.error),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.9,
+                  child: Text(result.name, ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.9,
+                  child: Text(result.species, ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width / 1.9,
+                  child: Text(result.gender, ),
+                ),
+              ],
+            )
+          ],
+        ),
+        
+      ),
     );
   }
 }
