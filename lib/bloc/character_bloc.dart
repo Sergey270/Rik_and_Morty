e0c040ab15b8140db3f445693469418c72d62aef
2707066 <80663695+Sergey270@users.noreply.github.com>
2023-07-09 22:36:04 +0600

    add BlocObservable

--- lib/bloc/character_bloc.dart
+++ lib/bloc/character_bloc.dart
@@ -15,12 +15,13 @@ class CharacterBloc extends Bloc<CharacterEvent, CharacterState> {
     on<CharacterEventFetch>((event, emit) async {
       emit(const CharacterState.loading());
       try{
-        Character _characterLoaded = await characterRepo.getCharacter(event.page, event.name);
+        Character _characterLoaded = await characterRepo.getCharacter(event.page, event.name).timeout(const Duration(seconds: 5));
       emit(CharacterState.loaded(characterLoaded: _characterLoaded));
 
 
       } catch(_){
         emit(const CharacterState.error());
+        rethrow;
 
       }
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rik_and_morty/ui/pages/home_page.dart';
 
 void main() {
+
   runApp(const MyApp());
 }
 
